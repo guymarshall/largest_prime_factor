7a8fa5ac1704fa4361b94764b5bc0e6f75d3a511
Guy Marshall <guymarshall9@gmail.com>
2022-07-02 21:10:36 +0100

    Get user input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 //The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
 
-
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter an integer:");
+
+    let mut user_input: String = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+
+    let number: i32 = user_input.trim().parse().expect("Please type a number!");
+
+    println!("You entered: {}", number);
 }
