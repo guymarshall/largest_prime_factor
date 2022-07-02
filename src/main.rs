5c8a7edaadd1849f40ee11f1f252a4b656409f90
Guy Marshall <guymarshall9@gmail.com>
2022-07-02 21:17:02 +0100

    Add get_user_input()

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@
 
 use std::io;
 
-fn main() {
-    println!("Please enter an integer:");
+fn get_user_input(prompt: &str) -> i32 {
+    println!("{}", prompt);
 
     let mut user_input: String = String::new();
 
     io::stdin().read_line(&mut user_input).expect("Failed to read line");
 
-    let number: i32 = user_input.trim().parse().expect("Please type a number!");
+    let number: i32 = user_input.trim().parse().expect("Please enter an integer!");
 
-    println!("You entered: {}", number);
+    return number;
+}
+
+fn main() {
+    let number: i32 = get_user_input("Please enter an integer:");
+
+    println!("{}", number);
 }
